2a7b74fbc99fcf5a926106107f955fb9ccac2c0c
Kerly2002 <kerlly.castillo.pastrana@utelvt.edu.ec>
2022-09-29 16:59:30 -0500

    examen

---
+++ compara.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//kerly jisley castilll pastrana 
+int main() 
+{
+	float k,P;
+	cout<<"ingrese el primer numero :   ";
+	cin>> k;
+	cout<<"ingrese el segundo numero :  ";
+	cin>> P;
+	if(k>P)
+	{
+		cout<<"el primer es mayor:  ";
+
+	}
+	
+	else if(k==P)
+	{
+		cout<<"son iguales:  ";
+	}
+	else if(k<P)
+	{  
+		cout<<"se el segundo es mayor:  ";
+	}
+
+	return(0);
+}
